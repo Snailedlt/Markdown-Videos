d22df90d803118fb57361dd2da5b158edf4523d8
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2023-06-30 18:09:33 +0200

    Add original max resolution image

--- main.py
+++ main.py
@@ -26,7 +26,11 @@ def youtube_thumbnail (
     height: int = 180,
     duration: int = Query(500, description="Duration for each image in the GIF (measured in milliseconds)"),
     ):
-    first, *following = (
+    first = util.add_play_button_to_thumbnail(
+                util.read_img_from_url(f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg").resize((width, height)),
+                f"img/youtube_play_button.png"
+            )
+    following = (
         util.add_play_button_to_thumbnail(
             util.read_img_from_url(f"https://i.ytimg.com/vi/{video_id}/{i+1}.jpg").resize((width, height)),
             f"img/youtube_play_button.png"
