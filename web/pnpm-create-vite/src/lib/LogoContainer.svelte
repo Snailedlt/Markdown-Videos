4881d442c765963acb952cc651af4bfc15b92024
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2023-08-04 14:06:43 +0200

    add basic UI

---
+++ web/pnpm-create-vite/src/lib/LogoContainer.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { LucideMinus, LucideYoutube } from 'lucide-svelte';
+</script>
+
+<div class="logo-container">
+  <img
+    src="https://deviconapi.vercel.app/markdown?color=ffffff&size=128"
+    class="logo markdown"
+    alt="Markdown Logo"
+  />
+  <span class="minus">-</span>
+  <span class="logo">
+    <LucideYoutube size="6rem" color="red" />
+  </span>
+</div>
+
+<style>
+  .logo-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
+  .logo-container > span.minus {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 3.2rem;
+    line-height: 1.1rem;
+    font-weight: bold;
+    height: 6rem;
+  }
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 6rem;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2rem #ff0000aa);
+  }
+  .logo.markdown {
+    margin-right: 0.2rem;
+  }
+  .logo.markdown:hover {
+    filter: drop-shadow(0 0 2rem #ffffffaa);
+  }
+</style>
---
+++ web/pnpm-create-vite/src/lib/SearchField.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  let urlValue: string = '';
+  let urlPlaceholder: string = 'https://youtu.be/dQw4w9WgXcQ';
+  let titleValue: string = '';
+  let titlePlaceholder: string = 'Definitely not a rickroll';
+</script>
+
+<div class="input-container">
+  <div>
+    <label for="url">URL</label>
+    <input name="url" value={urlValue} placeholder={urlPlaceholder} />
+  </div>
+  <div>
+    <label for="title">Title</label>
+    <input name="title" value={titleValue} placeholder={titlePlaceholder} />
+  </div>
+</div>
+
+<style lang="scss">
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0.8rem;
+    & > div {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 1rem;
+      min-width: 100%;
+      & > input {
+        min-width: 80%;
+        height: 2rem;
+        text-align: center;
+      }
+    }
+  }
+</style>
