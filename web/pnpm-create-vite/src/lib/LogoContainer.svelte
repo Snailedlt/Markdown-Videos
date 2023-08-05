34b48e2b943b6fc38fe7a53a643f458b681b9e8e
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2023-08-06 00:49:04 +0200

    Add new logo

--- web/pnpm-create-vite/src/lib/LogoContainer.svelte
+++ web/pnpm-create-vite/src/lib/LogoContainer.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-  import { LucideYoutube } from 'lucide-svelte';
+  import logo from '/logo.svg';
 </script>
 
 <div class="logo-container">
-  <img
-    src="https://deviconapi.vercel.app/markdown?color=ffffff&size=128"
-    class="logo markdown"
-    alt="Markdown Logo"
-  />
-  <span class="minus">-</span>
-  <span class="logo">
-    <LucideYoutube size="6rem" color="red" />
-  </span>
+  <img class="logo" src={logo} alt="logo" />
 </div>
 
 <style lang="scss">
@@ -20,15 +12,6 @@
     justify-content: center;
     align-items: center;
     flex-direction: row;
-    & > span.minus {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 3.2rem;
-      line-height: 1.1rem;
-      font-weight: bold;
-      height: 6rem;
-    }
     & .logo {
       display: flex;
       justify-content: center;
@@ -39,12 +22,6 @@
     }
     & .logo:hover {
       filter: drop-shadow(0 0 2rem #ff0000aa);
-    }
-    & .logo-container .logo.markdown {
-      margin-right: 0.2rem;
-    }
-    & .logo.markdown:hover {
-      filter: drop-shadow(0 0 2rem #ffffffaa);
     }
   }
 </style>
